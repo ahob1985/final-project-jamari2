cbb2042e39dc1738ba2303e25d44ed934be1915f
ahob85 <aaron@ahob85.com>
2021-07-31 02:40:19 +0000

    final project

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-// Author:
+console.log('ml5 version:', ml5.version)
 
-// Global UI Variables
-//let canvasDiv;
-
-// Global ML Variables
-
-function setup() {
-
+let soundClassifier;
+let resultP;
+function preload() {
+  let options = { probabilityThreshold: 0.95 };
+  soundClassifier = ml5.soundClassifier('SpeechCommands18w', options);
+  
 }
-
-function draw() {
-
+function setup() {
+  createCanvas(400, 400);
+  resultP = createP('waiting...');
+  resultP.style('font-size','32pt');
+  soundClassifier.classify(gotResults);
 }
 
 function gotResults(error, results) {
-
+  if (error) {
+    console.log('something went wrong');
+    console.error(error);
+  }
+  resultP.html(`${results[0].label} : ${results[0].confidence}`);
 }
+
